0da6a5951ed54367378f687641c52fb7fb339bcf
Vishal Kumar <amvishal09@outlook.com>
2023-02-18 07:47:11 +0530

    added index.html for / info

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const path = require("path");
 const port = 8000;
 const db = require("./config/mongoose");
 const indexRoutes = require("./routes/index");
+const fs = require("fs");
 
 const app = express();
 
@@ -15,6 +17,20 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Set up routes
 app.use("/", indexRoutes);
+
+// rendering index.html data
+app.get("/", (_req, res) => {
+  try {
+    return res.sendFile(path.join(__dirname, "index.html"));
+  } catch (err) {
+    const errorMessage = "Error found in loading. Please try again later.";
+    res.render("error", { errorMessage });
+  }
+});
+
+app.use((_req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "404.html"));
+});
 
 // Start server
 app.listen(port, () => console.log(`Server started on port ${port}`));
